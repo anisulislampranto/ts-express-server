c82ebea68fb077751b05d7f7f5a31d3820035f92
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-11-30 23:41:14 +0600

    postgress nion db connected

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,50 @@
 import express, { Request, Response } from 'express'
 import {Pool} from "pg"
-require('dotenv').config()
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({path: path.join(process.cwd(), '.env')})
 
 const app = express()
 const port = 5000
 
+// DB
 const pool = new Pool({
-    connectionString: process.env.psql
+    connectionString: process.env.DB_URI
 })
+
+const initDB = async() => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(150) NOT NULL,
+            age INT,
+            phone VARCHAR(15),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+    `)
+
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos (
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(), 
+            updated_ai TIMESTAMP DEFAULT NOW()
+        )
+    `)
+
+
+}
+
+initDB()
 
 // parser
 app.use(express.json())
