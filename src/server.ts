53174f5667b30715b047f02d23e2bb226245053f
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-12-02 23:51:06 +0600

    not found route

--- src/server.ts
+++ src/server.ts
@@ -224,6 +224,14 @@ app.get('/api/todos/', async(req: Request, res: Response) => {
     
 })
 
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found',
+        path: req.path
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
