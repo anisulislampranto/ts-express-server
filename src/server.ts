6fc4c70721747232bf06449e60781fc36a20145e
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-12-03 23:31:15 +0600

    config

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,12 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import {Pool} from "pg"
-import dotenv from 'dotenv'
-import path from 'path'
-
-dotenv.config({path: path.join(process.cwd(), '.env')})
+import config from './config'
+import initDB from './config/db';
 
 const app = express()
-const port = 5000
+const port = config.port;
 
-// DB
-const pool = new Pool({
-    connectionString: process.env.DB_URI
-})
-
-const initDB = async() => {
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(150) UNIQUE NOT NULL,
-            age INT,
-            phone VARCHAR(15),
-            address TEXT,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-    `)
-
-
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS todos (
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(), 
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-    `)
-
-
-}
-
+// int db
 initDB()
 
 // parser
@@ -51,7 +14,12 @@ app.use(express.json())
 // for formData 
 // app.use(express.urlencoded())/
 
-app.get('/', (req: Request, res: Response) => {
+const logger  = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+    next();
+}
+
+app.get('/', logger, (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
