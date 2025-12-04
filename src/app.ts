76d806ff0d4862a5824b788a12d919cee89ed643
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-12-04 23:56:53 +0600

    app.ts

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from 'express'
+import initDB from './config/db';
+import logger from './middleware/logger';
+import { userRoutes } from './modules/user/user.routes';
+import { todoRoutes } from './modules/todo/todo.routes';
+import { authRoutes } from './modules/auth/auth.routes';
+
+
+const app = express()
+
+
+// int db
+initDB()
+
+// parser
+app.use(express.json())
+// for formData 
+// app.use(express.urlencoded())/
+
+app.get('/', logger, (req: Request, res: Response) => {
+    res.send('Hello World!')
+})
+
+app.use('/api/users', userRoutes)
+
+// todos crud 
+app.post("/api/todos", todoRoutes)
+
+// auth routes 
+app.use('/api/auth', authRoutes)
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Route not found',
+        path: req.path
+    })
+})
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,7 @@
-import express, { NextFunction, Request, Response } from 'express'
-import config from './config'
-import initDB, { pool } from './config/db';
-import logger from './middleware/logger';
-import { userRoutes } from './modules/user/user.routes';
-import { todoRoutes } from './modules/todo/todo.routes';
-import { authRoutes } from './modules/auth/auth.routes';
+import app from "./app"
+import config from "./config"
 
-const app = express()
 const port = config.port;
-
-// int db
-initDB()
-
-// parser
-app.use(express.json())
-// for formData 
-// app.use(express.urlencoded())/
-
-app.get('/', logger, (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
-
-app.use('/api/users', userRoutes)
-
-// todos crud 
-app.post("/api/todos", todoRoutes)
-
-// auth routes 
-app.use('/api/auth', authRoutes)
-
-app.use((req, res) => {
-    res.status(404).json({
-        success: false,
-        message: 'Route not found',
-        path: req.path
-    })
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
